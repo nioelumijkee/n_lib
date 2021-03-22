c8b2e68696ffd4575a4e0bc0a589f95460b124cc
Nioelumijke <harmnine9@gmail.com>
2021-03-23 00:04:08 +0300

    not repeat...

--- n_random.c
+++ n_random.c
@@ -6,42 +6,49 @@ static t_class *n_random_class;
 typedef struct _n_random
 {
   t_object x_obj;
-  t_int seed;
-  t_int range;
-  t_int z;
+  unsigned int seed;
+  unsigned int range;
+  unsigned int z;
 } t_n_random;
 
 //----------------------------------------------------------------------------//
 void n_random_bang(t_n_random *x)
 {
-  time_t lt = time(NULL);
-  x->seed += lt + 3533;
+  unsigned int i;
   AF_RANDOM(x->seed);
-  t_int i;
-  if (x->seed < 0) i = 0 - x->seed;
-  else             i = x->seed;
-  i = i % x->range;
+  i = x->seed % x->range;
   /* not repeat */
-  i += x->z;
-  i = i % x->range;
-  outlet_float(x->x_obj.ob_outlet,(t_float)i);
+  if (i == x->z)
+    {
+      i += x->z;
+      i = i % x->range;
+    }
   x->z = i;
+  if (x->z == 0)
+    {
+      AF_RANDOM(x->seed);
+      x->z = x->seed % x->range;
+    }
+  outlet_float(x->x_obj.ob_outlet,(t_float)i);
 }
 
 //----------------------------------------------------------------------------//
 void n_random_float(t_n_random *x, t_floatarg f)
 {
-  int i = f;
-  if (i < 0)  i = 0 - i;
-  if (i == 0) i = 1;
-  x->range = i;
+  x->range = f;
+  if (x->range == 0) 
+    {    
+      x->range = 1;
+    }
 }
 
 //----------------------------------------------------------------------------//
 void *n_random_new(t_floatarg f)
 {
   t_n_random *x = (t_n_random *)pd_new(n_random_class);
+  time_t lt = time(NULL);
   x->seed = (long)x; /* random seed for varioius object in path */
+  x->seed += lt;
   n_random_float(x, f);
   outlet_new(&x->x_obj, 0);
   return (void *)x;
