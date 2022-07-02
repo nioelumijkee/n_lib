734ca450e4b308ccf2f70476491f180ed02500b3
Nioelumijke <harmnine9@gmail.com>
2022-07-02 03:23:18 +0300

    n_date: add time and st

--- n_date.c
+++ n_date.c
@@ -10,44 +10,61 @@ typedef struct _n_date
 //----------------------------------------------------------------------------//
 void n_date_bang(t_n_date *x)
 {
-  t_atom a[9];
-  struct tm *ptr;
+  t_atom a[14];
+  struct tm *s_tm;
   time_t lt;
   char s[40];
+
   lt = time(NULL);
-  ptr = localtime(&lt);
+  s_tm = localtime(&lt);
 
   // year
-  SETFLOAT(a, (t_float)ptr->tm_year + 1900);
+  SETFLOAT(a, (t_float)s_tm->tm_year + 1900);
 
   // month
-  SETFLOAT(a+1, (t_float)ptr->tm_mon + 1);
+  SETFLOAT(a+1, (t_float)s_tm->tm_mon + 1);
 
   // day
-  SETFLOAT(a+2, (t_float)ptr->tm_mday);
+  SETFLOAT(a+2, (t_float)s_tm->tm_mday);
 
   // hour
-  SETFLOAT(a+3, (t_float)ptr->tm_hour);
+  SETFLOAT(a+3, (t_float)s_tm->tm_hour);
 
   // min
-  SETFLOAT(a+4, (t_float)ptr->tm_min);
+  SETFLOAT(a+4, (t_float)s_tm->tm_min);
 
   // sec
-  SETFLOAT(a+5, (t_float)ptr->tm_sec);
+  SETFLOAT(a+5, (t_float)s_tm->tm_sec);
 
   // weekday
-  strftime(s, 40,"%A", ptr);
+  strftime(s, 40,"%A", s_tm);
   SETSYMBOL(a+6, gensym(s));
 
   // name month short
-  strftime(s, 40,"%b", ptr);
+  strftime(s, 40,"%b", s_tm);
   SETSYMBOL(a+7, gensym(s));
 
   // name month
-  strftime(s, 40,"%B", ptr);
+  strftime(s, 40,"%B", s_tm);
   SETSYMBOL(a+8, gensym(s));
 
-  outlet_list(x->x_obj.ob_outlet, &s_list, 9, a);
+  // day 0 - 365
+  SETFLOAT(a+9, (t_float)s_tm->tm_yday);
+
+  // summer time mode (Daylight Saving Time)
+  SETFLOAT(a+10, (t_float)s_tm->tm_isdst);
+
+  // amount system ticks per second
+  SETFLOAT(a+11, (t_float)CLOCKS_PER_SEC);
+
+  // time(sec)
+  SETFLOAT(a+12, (t_float)lt);
+
+  // time(sec) as symbol
+  sprintf(s, "%ld", lt);
+  SETSYMBOL(a+13, gensym(s));
+
+  outlet_list(x->x_obj.ob_outlet, &s_list, 14, a);
 }
 
 //----------------------------------------------------------------------------//
