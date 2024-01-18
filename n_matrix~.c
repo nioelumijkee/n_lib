2722fa8fbf8d3ce3fe7475da1f38bbe7f9896062
nioelumijke <harmnine9@gmail.com>
2024-01-18 12:42:43 +0300

    fix bug in n_matrix~. add a/m mode.

--- n_matrix~.c
+++ n_matrix~.c
@@ -1,77 +1,98 @@
 #include <string.h>
 #include "m_pd.h"
-#include "include/parsearg.h"
 
-#define MAX_IN  64 // MAX PD INLET ???
-#define MAX_OUT 64 // MAX PD OUTLET ???
-#define NOUSE(X) if(X){};
+#define MAX_IN  32 // MAX PD INLET ???
+#define MAX_OUT 32 // MAX PD OUTLET ???
+#define MAX_SEQ 1024 // MAX_IN * MAX_OUT
+#define MAX_VEC 66 // MAX_IN + MAX_OUT + 2
+#define MODE_ADD 0
+#define MODE_MULT 1
+
+#define _clip_minmax(min, max, v) v=(v>max)?max:(v<min)?min:v;
+#define _clip_min(min, v) v=(v<min)?min:v;
+#define _clip_max(max, v) v=(v>max)?max:v;
+
+#define NOUSE(x) if(x){};
 
 static t_class *n_matrix_class;
 
 typedef struct _n_matrix
 {
   t_object x_obj;
-  t_int n;          /* block size */
-  t_int all_in;     /* number in's */
-  t_int all_out;    /* number out's */
-  t_int all;        /* in's + out's */
-  t_int **v_d;      /* vector for dsp_addv */
-  t_float **l; /* level's */
-  int *seq_in;
-  int *seq_out;
-  /* t_float l[MAX_IN][MAX_OUT]; /\* level's *\/ */
-  /* int seq_in[MAX_SEQ]; */
-  /* int seq_out[MAX_SEQ]; */
-  int seq_len;
+  t_int bs;                     // block size 
+  t_int all_in;                 // number in's 
+  t_int all_out;                // number out's 
+  t_int all;                    // in's + out's 
+  t_int *v_d[MAX_VEC];          // vector for dsp_addv 
+  t_float l[MAX_IN][MAX_OUT];   // level's 
+  t_int s_in[MAX_SEQ];          // seq in
+  t_int s_out[MAX_SEQ];         // seq out
+  t_int s_len;                  // seq length
+  int mode;
 } t_n_matrix;
 
-t_int *n_matrix_perform(t_int *w)
+t_int *n_matrix_perform_add(t_int *w)
 {
-  int i;
+  int i,j;
   t_n_matrix *x = (t_n_matrix *)(w[1]);
-  t_sample *sig_in[MAX_IN];  /* vector in's */
-  t_sample *sig_out[MAX_OUT]; /* vector out's */
+  t_sample *sig_in[MAX_IN];
+  t_sample *sig_out[MAX_OUT]; 
   for (i = 0; i < x->all_in; i++)
-    {
-      sig_in[i] = (t_sample *)(w[2 + i]);
-    }
+    sig_in[i] = (t_sample *)(w[2 + i]);
   for (i = 0; i < x->all_out; i++)
-    {
-      sig_out[i] = (t_sample *)(w[2 + i + x->all_in]);
-    }
-  int n;
-  int n_in, n_out;
+    sig_out[i] = (t_sample *)(w[2 + i + x->all_in]);
   t_float buf[MAX_IN];
-  n = x->n;
-  while (n--)
+  for (i=0; i<x->bs; i++)
     {
       // copy to buffer
-      for (n_in=0; n_in<x->all_in; n_in++)
-	buf[n_in] = *(sig_in[n_in]++);
+      for (j=0; j<x->all_in; j++)
+	buf[j] = sig_in[j][i];
       // clear outs
-      for (n_out=0; n_out<x->all_out; n_out++)
-	*(sig_out[n_out]) = 0.0;
+      for (j=0; j<x->all_out; j++)
+	sig_out[j][i] = 0.0;
       // seq
-      for (i=0; i<x->seq_len; i++)
-        {
-          *(sig_out[x->seq_out[i]]) += buf[x->seq_in[i]] *
-            x->l[x->seq_in[i]][x->seq_out[i]];
-        }
-      // out
-      for (n_out=0; n_out<x->all_out; n_out++)
-	sig_out[n_out]++;
+      for (j=0; j<x->s_len; j++)
+	sig_out[x->s_out[j]][i]+=buf[x->s_in[j]];
+    }
+  return (w + x->all + 2);
+}
+
+t_int *n_matrix_perform_mult(t_int *w)
+{
+  int i,j;
+  t_n_matrix *x = (t_n_matrix *)(w[1]);
+  t_sample *sig_in[MAX_IN];
+  t_sample *sig_out[MAX_OUT]; 
+  for (i = 0; i < x->all_in; i++)
+    sig_in[i] = (t_sample *)(w[2 + i]);
+  for (i = 0; i < x->all_out; i++)
+    sig_out[i] = (t_sample *)(w[2 + i + x->all_in]);
+  t_float buf[MAX_IN];
+  for (i=0; i<x->bs; i++)
+    {
+      // copy to buffer
+      for (j=0; j<x->all_in; j++)
+	buf[j] = sig_in[j][i];
+      // clear outs
+      for (j=0; j<x->all_out; j++)
+	sig_out[j][i] = 0.0;
+      // seq
+      for (j=0; j<x->s_len; j++)
+	sig_out[x->s_out[j]][i]+=buf[x->s_in[j]] * x->l[x->s_in[j]][x->s_out[j]];
     }
   return (w + x->all + 2);
 }
 
 static void n_matrix_dsp(t_n_matrix *x, t_signal **sp)
 {
-  int i;
-  x->n = sp[0]->s_n;
+  x->bs = sp[0]->s_n;
   x->v_d[0] = (t_int *)x;
-  for (i = 0; i < x->all; i++)
+  for (int i = 0; i < x->all; i++)
     x->v_d[i + 1] = (t_int *)sp[i]->s_vec;
-  dsp_addv(n_matrix_perform, x->all + 1, (t_int *)x->v_d);
+  if (x->mode==MODE_ADD)
+    dsp_addv(n_matrix_perform_add, x->all + 1, (t_int *)x->v_d);
+  else
+    dsp_addv(n_matrix_perform_mult, x->all + 1, (t_int *)x->v_d);
 }
 
 static void n_matrix_debug(t_n_matrix *x)
@@ -86,7 +107,7 @@ static void n_matrix_debug(t_n_matrix *x)
       buf[0] = '\0';
       for (n_out=0; n_out<x->all_out; n_out++)
         {
-          sprintf(b,"%s%5g | ",buf,x->l[n_in][n_out]);
+          sprintf(b,"%s%-4.2f|",buf,x->l[n_in][n_out]);
           strcpy(buf,b);
         }
       post(buf);
@@ -95,86 +116,78 @@ static void n_matrix_debug(t_n_matrix *x)
 
 static void n_matrix_calc_seq(t_n_matrix *x)
 {
-  int n_in, n_out;
-  x->seq_len = 0;
-  for (n_out=0; n_out<x->all_out; n_out++)
+  x->s_len = 0;
+  for (int out=0; out<x->all_out; out++)
     {
-      for (n_in=0; n_in<x->all_in; n_in++)
+      for (int in=0; in<x->all_in; in++)
         {
-          if (x->l[n_in][n_out] != 0)
+          if (x->l[in][out] != 0)
             {
-              x->seq_in[x->seq_len] = n_in;
-              x->seq_out[x->seq_len] = n_out;
-              x->seq_len++;
+              x->s_in[x->s_len] = in;
+              x->s_out[x->s_len] = out;
+              x->s_len++;
             }
         }
     }
 }
 
-static void n_matrix_list(t_n_matrix *x, t_symbol *s, int ac, t_atom *av)
+static void n_matrix_set(t_n_matrix *x, t_floatarg in, t_floatarg out, t_floatarg l)
 {
-  int n_in  = atom_getfloatarg(0,ac,av);
-  int n_out = atom_getfloatarg(1,ac,av);
-  t_float l = atom_getfloatarg(2,ac,av);
-  if (n_in  >= 0  &&  n_in  < x->all_in &&
-      n_out >= 0  &&  n_out < x->all_out)
-    x->l[n_in][n_out] = l;
+  if (in  >= 0  &&
+      in  < x->all_in &&
+      out >= 0  &&
+      out < x->all_out)
+    x->l[(int)in][(int)out] = l;
   n_matrix_calc_seq(x);
-  if (s) {};
 }
 
 static void *n_matrix_new(t_symbol *s, int ac, t_atom *av)
 { 
-  int i,j;
   t_n_matrix *x = (t_n_matrix *)pd_new(n_matrix_class);
-  x->all_in  = atom_getfloatarg(0,ac,av);
-  x->all_out = atom_getfloatarg(1,ac,av);
-  x->all_in  = (x->all_in>MAX_IN)?MAX_IN:(x->all_in<1)?1:x->all_in;
-  x->all_out = (x->all_out>MAX_OUT)?MAX_OUT:(x->all_out<1)?1:x->all_out;
-  x->all = x->all_in + x->all_out;
-  for (i = 1; i < x->all_in; i++)
-    inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
-  for (i = 0; i < x->all_out; i++)
-    outlet_new(&x->x_obj, &s_signal);
-  // seq
-  x->seq_in = getbytes(sizeof(t_int) * x->all_in);
-  x->seq_out = getbytes(sizeof(t_int) * x->all_out);
-  x->l = getbytes(sizeof(t_float *) * x->all_in);
-  for (i=0; i<x->all_in; i++)
+  if (ac >= 1)
     {
-      x->l[i] = getbytes(sizeof(t_float) * x->all_out);
-      for (j=0; j<x->all_out; j++)
-	{
-	  x->l[i][j] = 0.0;
-	}
+      t_symbol *mode = atom_getsymbolarg(0,ac,av);
+      if (mode->s_name[0] == 'a')
+	x->mode=MODE_ADD;
+      else
+	x->mode=MODE_MULT;
     }
-  x->v_d = getbytes(sizeof(t_int *) *(x->all + 2));
+  else
+    x->mode=MODE_MULT;
+  if (ac >= 2)
+    {
+      x->all_in  = atom_getfloatarg(1,ac,av);
+      _clip_minmax(1, MAX_IN, x->all_in);
+    }
+  else
+    x->all_in = 1;
+  if (ac >= 3)
+    {
+      x->all_out = atom_getfloatarg(2,ac,av);
+      _clip_minmax(1, MAX_OUT, x->all_in);
+    }
+  else
+    x->all_out = 1;
+  x->all = x->all_in + x->all_out;
+  // create inlet/outlet
+  for (int i = 1; i < x->all_in; i++)
+    inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
+  for (int i = 0; i < x->all_out; i++)
+    outlet_new(&x->x_obj, &s_signal);
   return (x);
   NOUSE(s);
-}
-
-static void n_matrix_free(t_n_matrix *x)
-{
-  int i;
-  freebytes(x->v_d, sizeof(t_int *) * (x->all + 2));
-  freebytes(x->seq_in, sizeof(t_int) * x->all_in);
-  freebytes(x->seq_out, sizeof(t_int) * x->all_out);
-  for (i=0; i<x->all_in; i++)
-    {
-      freebytes(x->l[i], sizeof(t_float) * x->all_out);
-    }
-  freebytes(x->l, sizeof(t_float *) * x->all_in);
 }
 
 void n_matrix_tilde_setup(void)
 {
   n_matrix_class = class_new(gensym("n_matrix~"),
 			     (t_newmethod)n_matrix_new, 
-                             (t_method)n_matrix_free, 
+                             0,
                              sizeof(t_n_matrix),
 			     0, A_GIMME, 0);
   class_addmethod(n_matrix_class, nullfn, gensym("signal"), 0);
   class_addmethod(n_matrix_class, (t_method)n_matrix_dsp, gensym("dsp"), 0);
-  class_addlist(n_matrix_class, (t_method)n_matrix_list);
+  class_addmethod(n_matrix_class, (t_method)n_matrix_set, gensym("set"), 
+		  A_FLOAT, A_FLOAT, A_FLOAT, 0);
   class_addmethod(n_matrix_class, (t_method)n_matrix_debug,gensym("debug"),0);
 }
